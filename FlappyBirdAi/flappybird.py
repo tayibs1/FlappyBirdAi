27fd98c2e4a521cf68bdcba7f84f692d7e7b209a
Tayib <zmac596@live.rhul.ac.uk>
2024-12-23 23:28:47 +0000

    Added Pipe class with basic drawing functionality

--- FlappyBirdAi/flappybird.py
+++ FlappyBirdAi/flappybird.py
@@ -90,6 +90,30 @@ class Bird:
         
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
+    
+# Add the Pipe class for basic drawing functionality.
+class Pipe:
+    GAP = 200
+    VEL = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50, 450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    def draw(self, win):
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
 
 def draw_window(win, bird):
     # .blit function means .draw, and draws the background img from the top-left
